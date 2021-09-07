e7730a2a08520623efd4a7995f05edfa0b7bc210
hristinah <89635038+hristinah@users.noreply.github.com>
2021-09-07 19:06:30 +0300

    solutions updated

--- prizes_admin.php
+++ prizes_admin.php
@@ -9,7 +9,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Награди</title>
-    <link rel="icon" href="logo.jpg" type="image/jpg">
     <link href="https://fonts.googleapis.com/css?family=Oswald&display=swap" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="style.css?<?php echo time(); ?>">
 
